04cac8e1ab54b9391f1dcca6f60aef4eaa7573ed
LK-LINK <ke.liful@gmail.com>
2025-05-28 10:14:30 +0800

    feat: 添加解码 Opus 数据到 CMSampleBuffer 的方法

--- Sources/Opus/Opus.Decoder.swift
+++ Sources/Opus/Opus.Decoder.swift
@@ -1,5 +1,6 @@
 import AVFoundation
 import Copus
+import CoreMedia
 
 extension Opus {
 	public class Decoder {
@@ -101,5 +102,119 @@ extension Opus.Decoder {
 			throw Opus.Error(decodedCount)
 		}
 		return Int(decodedCount)
+	}
+}
+
+// MARK: CMSampleBuffer decode methods
+
+extension Opus.Decoder {
+	/// 解码 Opus 数据到 CMSampleBuffer
+	/// - Parameter input: 输入的 Opus 编码数据
+	/// - Returns: 解码后的 CMSampleBuffer
+	/// - Throws: Opus.Error 解码过程中的错误
+	public func decodeToCMSampleBuffer(_ input: Data) throws -> CMSampleBuffer {
+		let audioBuffer = try decode(input)
+		return try createCMSampleBuffer(from: audioBuffer)
+	}
+
+	/// 解码 Opus 数据到 CMSampleBuffer
+	/// - Parameters:
+	///   - input: 输入的 Opus 编码数据缓冲区
+	///   - presentationTimeStamp: 显示时间戳
+	/// - Returns: 解码后的 CMSampleBuffer
+	/// - Throws: Opus.Error 解码过程中的错误
+	public func decodeToCMSampleBuffer(_ input: Data, presentationTimeStamp: CMTime = CMTime.zero) throws -> CMSampleBuffer {
+		let audioBuffer = try decode(input)
+		return try createCMSampleBuffer(from: audioBuffer, presentationTimeStamp: presentationTimeStamp)
+	}
+
+	/// 从 AVAudioPCMBuffer 创建 CMSampleBuffer
+	/// - Parameters:
+	///   - audioBuffer: 音频PCM缓冲区
+	///   - presentationTimeStamp: 显示时间戳，默认为零
+	/// - Returns: CMSampleBuffer
+	/// - Throws: Opus.Error 转换过程中的错误
+	private func createCMSampleBuffer(from audioBuffer: AVAudioPCMBuffer, presentationTimeStamp: CMTime = CMTime.zero) throws -> CMSampleBuffer {
+		let audioBufferList = audioBuffer.audioBufferList.pointee
+
+		// 创建音频格式描述
+		var audioFormatDescription: CMAudioFormatDescription?
+		let status = CMAudioFormatDescriptionCreate(
+			allocator: kCFAllocatorDefault,
+			asbd: audioBuffer.format.streamDescription,
+			layoutSize: 0,
+			layout: nil,
+			magicCookieSize: 0,
+			magicCookie: nil,
+			extensions: nil,
+			formatDescriptionOut: &audioFormatDescription
+		)
+
+		guard status == noErr, let formatDescription = audioFormatDescription else {
+			throw Opus.Error.internalError
+		}
+
+		// 创建 CMBlockBuffer 来存储音频数据
+		var blockBuffer: CMBlockBuffer?
+		let frameCount = Int(audioBuffer.frameLength)
+		let bytesPerFrame = audioBuffer.format.streamDescription.pointee.mBytesPerFrame
+		let dataSize = frameCount * Int(bytesPerFrame)
+
+		let blockBufferStatus = CMBlockBufferCreateWithMemoryBlock(
+			allocator: kCFAllocatorDefault,
+			memoryBlock: nil,
+			blockLength: dataSize,
+			blockAllocator: kCFAllocatorDefault,
+			customBlockSource: nil,
+			offsetToData: 0,
+			dataLength: dataSize,
+			flags: 0,
+			blockBufferOut: &blockBuffer
+		)
+
+		guard blockBufferStatus == noErr, let buffer = blockBuffer else {
+			throw Opus.Error.internalError
+		}
+
+		// 将音频数据复制到 CMBlockBuffer
+		let copyStatus = CMBlockBufferReplaceDataBytes(
+			with: audioBufferList.mBuffers.mData!,
+			blockBuffer: buffer,
+			offsetIntoDestination: 0,
+			dataLength: dataSize
+		)
+
+		guard copyStatus == noErr else {
+			throw Opus.Error.internalError
+		}
+
+		// 创建样本缓冲区
+		var sampleBuffer: CMSampleBuffer?
+		var sampleTiming = CMSampleTimingInfo(
+			duration: CMTime(value: CMTimeValue(frameCount), timescale: Int32(audioBuffer.format.sampleRate)),
+			presentationTimeStamp: presentationTimeStamp,
+			decodeTimeStamp: CMTime.invalid
+		)
+
+		let createStatus = CMSampleBufferCreate(
+			allocator: kCFAllocatorDefault,
+			dataBuffer: buffer,
+			dataReady: true,
+			makeDataReadyCallback: nil,
+			refcon: nil,
+			formatDescription: formatDescription,
+			sampleCount: frameCount,
+			sampleTimingEntryCount: 1,
+			sampleTimingArray: &sampleTiming,
+			sampleSizeEntryCount: 0,
+			sampleSizeArray: nil,
+			sampleBufferOut: &sampleBuffer
+		)
+
+		guard createStatus == noErr, let finalBuffer = sampleBuffer else {
+			throw Opus.Error.internalError
+		}
+
+		return finalBuffer
 	}
 }
